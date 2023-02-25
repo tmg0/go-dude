cfdb419e2c70c60246eb0dfe8b22489bd7fff754
zekun.jin <zekun.jin@foxmail.com>
2023-02-25 20:16:54 +0800

    feat: add command to entry file

---
+++ index.ts
@@ -0,0 +1,40 @@
+#! /usr/bin/env node
+
+import { NodeSSH } from 'node-ssh'
+import { program } from 'commander'
+import YAML from 'yaml'
+import { version } from './package.json'
+import { readConf, getProjectName, getDockerComposeFilePath, throwError } from './src/utils'
+
+program.command('push')
+  .version(version)
+  .description('Push image to docker-compose file by ssh.')
+  .argument('<string>', 'Image URL')
+  .option('-c --config <char>', 'Declare dude config file.')
+  .action(async (str, option) => {
+    const config = await readConf(option.config)
+
+    if (!config) { throwError('Do not exist available config file.') }
+
+    const name = getProjectName(config)
+
+    if (!name) { throwError('Please declare a project name in config file or package.json.') }
+
+    const ssh = new NodeSSH()
+
+    try {
+      await ssh.connect({ ...config.ssh })
+
+      const { stdout: yml } = await ssh.execCommand(`cat ${config.dockerCompose.file}`)
+      const json = YAML.parse(yml)
+      const { image } = json.services[name]
+      await ssh.execCommand(`sed -i 's|${image}|${str}|g' ${config.dockerCompose.file}`)
+      await ssh.execCommand(`cd ${getDockerComposeFilePath(config)} && docker-compose up -d ${name}`)
+    } catch (error) {
+      throwError(error)
+    } finally {
+      ssh.dispose()
+    }
+  })
+
+program.parse(process.argv)
--- src/utils.ts
+++ src/utils.ts
@@ -3,14 +3,13 @@ import consola from 'consola'
 import { readJSONSync } from 'fs-extra'
 import { CONFIG_FILENAME } from './consts'
 
-export const getConfigFilepath = (path: string) => join(process.cwd(), path, CONFIG_FILENAME)
-
 export const getProjectName = (config: DudeConfig) => config.name || readJSONSync(join(process.cwd(), 'package.json')).name
 
 export const getDockerComposeFilePath = (config: DudeConfig) => dirname(config.dockerCompose.file)
 
 export const readConf = (path = '.'): Promise<DudeConfig> => {
-  return readJSONSync(getConfigFilepath(path))
+  path = join(process.cwd(), path, CONFIG_FILENAME)
+  return readJSONSync(path)
 }
 
 export const throwError = (error: any) => {
