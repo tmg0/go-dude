5029f61f7f61cd00e051f917df181bf69c7937fe
zekunjin <zekun.jin@foxmail.com>
2023-02-28 16:24:03 +0800

    feat: add ssh connect log

--- src/shared/ssh.ts
+++ src/shared/ssh.ts
@@ -1,8 +1,10 @@
 import { NodeSSH } from 'node-ssh'
+import consola from 'consola'
 
 const ssh = new NodeSSH()
 
 export const sshConnect = async (config: DudeConfig) => {
   await ssh.connect({ ...config.ssh })
+  consola.success(`SSH connect complete. Host: ${config.ssh.host}`)
   return ssh
 }
