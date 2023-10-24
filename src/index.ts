93490c3ca85acfa37e0676eee73a00487b25738c
zekunjin <zekun.jin@foxmail.com>
2023-10-24 14:48:01 +0800

    chore: move sub cmd position

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,8 @@ import { name, version } from '../package.json'
 const main = defineCommand({
   meta: { name, version, description: 'Dude CLI' },
   subCommands: {
-    push: () => import('./commands/push').then(r => r.default),
     build: () => import('./commands/build').then(r => r.default),
+    push: () => import('./commands/push').then(r => r.default),
     check: () => import('./commands/check').then(r => r.default)
   }
 })
