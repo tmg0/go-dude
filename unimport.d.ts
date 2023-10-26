491a9680d55be743f5acbb8f698fc454b0273310
zekunjin <zekun.jin@foxmail.com>
2023-10-26 12:04:59 +0800

    chore: use ctizen

---
+++ unimport.d.ts
@@ -0,0 +1,13 @@
+export {}
+declare global {
+  const build: typeof import('C:/Users/zekun.jin/Documents/Projects/go-dude/src/commands/build')['default']
+  const check: typeof import('C:/Users/zekun.jin/Documents/Projects/go-dude/src/commands/check')['default']
+  const defineCommand: typeof import('citty')['defineCommand']
+  const load: typeof import('C:/Users/zekun.jin/Documents/Projects/go-dude/src/commands/load')['default']
+  const parseArgs: typeof import('citty')['parseArgs']
+  const push: typeof import('C:/Users/zekun.jin/Documents/Projects/go-dude/src/commands/push')['default']
+  const renderUsage: typeof import('citty')['renderUsage']
+  const run: typeof import('C:/Users/zekun.jin/Documents/Projects/go-dude/src/commands/push')['run']
+  const runCommand: typeof import('citty')['runCommand']
+  const showUsage: typeof import('citty')['showUsage']
+}
