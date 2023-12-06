13f8f66787e7b913a66881053da7e9fe6fe76ddc
zekunjin <zekun.jin@foxmail.com>
2023-12-06 10:16:30 +0800

    chore: rm unimport declare file

--- utils/docker/index.ts
+++ utils/docker/index.ts
@@ -240,20 +240,4 @@ export const dockerPs = (name: string) => async (ssh?: NodeSSH) => {
   return stdout.split('\n').map(item => destr<DockerPs>(item)).filter(Boolean).filter(({ Names }) => Names?.includes(name)) || []
 }
 
-export const ensureDockerComposeFile = (config: DudeConfig) => async (ssh?: NodeSSH) => {
-  if (!ssh) { return }
-  if (!config?.dockerCompose) { throw config }
-  const [remoteExist, templateExist] = await Promise.all([isFileExist(config.dockerCompose.file)(ssh), isFileExist(getDockerComposeFileName(config))()])
-  if (!remoteExist && templateExist) {
-    const confirmed = await consola.prompt(`Do not exist docker compose file under ${getDockerComposeFilePath(config)}, create a new file from template?`, {
-      type: 'confirm'
-    })
-
-    if (confirmed) {
-      await sshExecAsync(ssh, `touch ${config.dockerCompose.file}`)
-      return
-    }
-
-    throw new Error(`Can not find the docker compose file: ${config.dockerCompose.file}`)
-  }
-}
+export const isDockerRunning = async () => {}
