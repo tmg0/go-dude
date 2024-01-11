2bc6fc8142f8a5d6832361062624a8aeb91a8677
zekunjin <zekun.jin@foxmail.com>
2024-01-11 11:36:05 +0800

    chore: add node version check

---
+++ commands/node.ts
@@ -0,0 +1,13 @@
+export default defineCommand({
+  meta: { name: 'node', description: 'Node infomation check.' },
+  args: {
+    version: { type: 'string', alias: 'v', description: 'Node version.' }
+  },
+  async run ({ args }) {
+    await checkVersion()
+    if (args.version) {
+      const data = await execAsync('node -v')
+      consola.info(data)
+    }
+  }
+})
